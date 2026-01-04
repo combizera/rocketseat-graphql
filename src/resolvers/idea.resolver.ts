c380472d850b1daedaa9cff3e35110664e7edf59
combizera <ycombi349@gmail.com>
2026-01-04 11:28:09 -0300

    feat: add Idea creation and update functionality with corresponding inputs and resolver

--- src/resolvers/idea.resolver.ts
+++ src/resolvers/idea.resolver.ts
@@ -1,13 +1,36 @@
-import { Arg, Mutation, Resolver } from "type-graphql";
+import { Arg, FieldResolver, Mutation, Resolver, Root, UseMiddleware } from "type-graphql";
 import { IdeaModel } from "../models/idea.model";
-import { CreateIdeaInput } from "../dtos/input/idea.input";
+import { CreateIdeaInput, UpdateIdeaInput } from "../dtos/input/idea.input";
+import { IdeaService } from "../services/idea.service";
+import { GqlUser } from "../graphql/decorators/user.decorator";
+import { IsAuth } from "../middlewares/auth.middleware";
+import { userService } from "../services/user.service";
+import { UserModel } from "../models/user.model";
 
 @Resolver(() => IdeaModel)
+@UseMiddleware(IsAuth)
 export class IdeaResolver {
+  private ideaService = new IdeaService();
+  private userService = new userService();
+
   @Mutation(() => IdeaModel)
   async createIdea(
-    @Arg('data', () => CreateIdeaInput) data: CreateIdeaInput
+    @Arg('data', () => CreateIdeaInput) data: CreateIdeaInput,
+    @GqlUser() user: UserModel
   ): Promise<IdeaModel> {
-    return this.ideaService.createIdea(data);
+    return this.ideaService.createIdea(data, user.id);
+  }
+
+  @Mutation(() => IdeaModel)
+  async updateIdea(
+    @Arg('id', () => String) id: string,
+    @Arg('data', () => UpdateIdeaInput) data: UpdateIdeaInput,
+  ): Promise<IdeaModel> {
+    return this.ideaService.updateIdea(id, data);
+  }
+
+  @FieldResolver(() => UserModel)
+  async author(@Root() idea: IdeaModel): Promise<UserModel> {
+    return this.userService.findUser(idea.authorId);
   }
 }
--- src/services/idea.service.ts
+++ src/services/idea.service.ts
@@ -1,14 +1,34 @@
-import { CreateIdeaInput } from "../dtos/input/idea.input";
+import { CreateIdeaInput, UpdateIdeaInput } from "../dtos/input/idea.input";
 import { prismaClient } from "../libs/prisma";
 
 export class IdeaService {
-   async createIdea(data: CreateIdeaInput, authorId: string) {
+  async createIdea(data: CreateIdeaInput, authorId: string) {
     return prismaClient.idea.create({
       data: {
         title: data.title,
         description: data.description,
         authorId: authorId,
       },
+    })
+  }
+
+  async updateIdea(id: string, data: UpdateIdeaInput) {
+    const idea = await prismaClient.idea.findUnique({
+      where: {
+        id: id
+      }
+    })
+
+    if (!idea) throw new Error("Idea not found");
+
+    return prismaClient.idea.update({
+      where: {
+        id: id
+      },
+      data: {
+        title: data.title,
+        description: data.description,
+      }
     })
   }
 }
