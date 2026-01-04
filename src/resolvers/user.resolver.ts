578fb89f5d44d17ecce4feaa89e8752770d5fd55
combizera <ycombi349@gmail.com>
2026-01-04 10:56:03 -0300

    feat: update user model to allow nullable password and implement user creation logic

--- src/resolvers/user.resolver.ts
+++ src/resolvers/user.resolver.ts
@@ -1,12 +1,20 @@
-import { Arg, Query, Resolver, UseMiddleware } from "type-graphql";
+import { Arg, Mutation, Query, Resolver, UseMiddleware } from "type-graphql";
 import { UserModel } from "../models/user.model";
 import { userService } from "../services/user.service";
 import { IsAuth } from "../middlewares/auth.middleware";
+import { CreateUserInput } from "../dtos/input/user.input";
 
 @Resolver(() => UserModel) 
 @UseMiddleware(IsAuth)
 export class UserResolver {
   private userService = new userService();
+
+  @Mutation(() => UserModel)
+  async createUser(
+    @Arg('data', () => CreateUserInput) data: CreateUserInput
+  ): Promise<UserModel> {
+    return this.userService.createUser(data);
+  }
 
   @Query(() => UserModel)
   async getUser(
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,6 +1,24 @@
+import { CreateUserInput } from "../dtos/input/user.input";
 import { prismaClient } from "../libs/prisma";
 
 export class userService {
+  async createUser(data: CreateUserInput) {
+    const findUser = await prismaClient.user.findUnique({
+      where: {
+        email: data.email
+      }
+    })
+
+    if (findUser) throw new Error("User already exists");
+
+    return prismaClient.user.create({
+      data: {
+        name: data.name,
+        email: data.email
+      }
+    })
+  }
+
   async findUser(id: string){
     const user = await prismaClient.user.findUnique({
       where: { id }
